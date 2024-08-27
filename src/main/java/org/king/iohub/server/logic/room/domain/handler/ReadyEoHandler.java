c3362a5580609178a0fe4fc418965350cb532e04
SaltedFishKnight <105956232+SaltedFishKnight@users.noreply.github.com>
2024-08-28 02:06:40 +0800

    向对战双方发送纳秒的时间戳，类似于“起跑线”，使对战双方同时开始战斗

--- src/main/java/org/king/iohub/server/logic/room/domain/handler/ReadyEoHandler.java
+++ src/main/java/org/king/iohub/server/logic/room/domain/handler/ReadyEoHandler.java
@@ -1,15 +1,16 @@
 package org.king.iohub.server.logic.room.domain.handler;
 
-import com.iohao.game.action.skeleton.core.BarMessageKit;
 import com.iohao.game.action.skeleton.core.CmdInfo;
 import com.iohao.game.action.skeleton.core.commumication.BroadcastContext;
-import com.iohao.game.action.skeleton.protocol.ResponseMessage;
+import com.iohao.game.action.skeleton.protocol.wrapper.WrapperKit;
 import com.iohao.game.bolt.broker.core.client.BrokerClientHelper;
 import com.iohao.game.widget.light.domain.event.message.DomainEventHandler;
 import org.king.iohub.common.route.RoomCmd;
 import org.king.iohub.server.logic.room.combat.CombatRoom;
 import org.king.iohub.server.logic.room.combat.CombatRoomService;
 import org.king.iohub.server.logic.room.domain.message.ReadyEo;
+
+import java.time.Instant;
 
 public class ReadyEoHandler implements DomainEventHandler<ReadyEo> {
 
@@ -27,8 +28,12 @@ public class ReadyEoHandler implements DomainEventHandler<ReadyEo> {
         if (combatRoom.isReadyPlayers() && combatRoom.isCombatStarted.compareAndSet(false, true)) {
             CmdInfo allPlayersReadyCmdInfo = CmdInfo.of(RoomCmd.cmd, RoomCmd.allPlayersReady);
             BroadcastContext broadcastContext = BrokerClientHelper.getBroadcastContext();
-            ResponseMessage responseMessage = BarMessageKit.createResponseMessage(allPlayersReadyCmdInfo);
-            broadcastContext.broadcast(responseMessage, combatRoom.listPlayerId());
+
+            Instant now = Instant.now();
+            // 计算从 Unix Epoch 开始的纳秒数，并将其延后 1 秒
+            long startTimeNano = now.getEpochSecond() * 1_000_000_000L + now.getNano() + 1_000_000_000L;
+
+            broadcastContext.broadcast(allPlayersReadyCmdInfo, WrapperKit.of(startTimeNano), combatRoom.listPlayerId());
         }
     }
 }
